f8ddae279e22a84a127176a7e7ea394c52a87682
bhuvan <bhuvanchebrolu@gmail.com>
2026-01-09 07:32:57 +0530

    deployemnt config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cors from "cors"
 import {serve} from "inngest/express"
 
+const __dirname=path.resolve();
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { functions, inngest } from "./lib/inngest.js";
@@ -22,7 +24,17 @@ app.use("/api/inngest",serve({client:inngest,functions}));
 app.get("/health",(req,res)=>{
     res.status(200).json({msg:"api is up and running"});
 })
+app.get("/books",(req,res)=>{
+    res.status(200).json({msg:"this is the books endpoint"})
+})
 
+if(ENV.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 
 const startServer=async()=>{
